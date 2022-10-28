3ab186189caaa89c1e7b8031dd74f3c673fc190b
Sebastian Zapata <sebastian.zapata@geniussports.com>
2022-10-27 20:21:09 -0500

    removing process .exit()

--- src/controller/bikes.integration.test.js
+++ src/controller/bikes.integration.test.js
@@ -8,47 +8,45 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 //Our parent block
-describe("Books", () => {
-  /*
-   * Test the /GET route
-   */
-  describe("/GET Bikes", () => {
-    it("it should GET all the Bikes", (done) => {
-      try {
-        chai
-          .request(server)
-          .get("/")
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a("array");
-            done();
-          });
-      } catch (error) {
-        console.log("error on test get all" + error);
-        done(new Error("Test failed to start"));
-      }
-    });
 
-    it("it should not Create a bike without id ", (done) => {
-      let bike = {
-        color: "blue",
-        model: "34",
-      };
-      try {
-        chai
-          .request(server)
-          .post("/")
-          .send(bike)
-          .end((err, res) => {
-            console.log(JSON.stringify(res));
-            res.should.have.status(405);
-            res.body.should.be.a("object");
-            done();
-          });
-      } catch (error) {
-        console.log("error on test get all" + error);
-        done(new Error("Test failed to start"));
+/*
+ * Test the /GET route
+ */
+
+describe("/GET Bikes", () => {
+  
+  it("it should GET all the Bikes", (done) => {
+    try{
+
+    
+    chai
+      .request(server)
+      .get("/")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        done();
+      });
+    }catch(erorr){
+        console.log("eeeeeeeeeeeror")
       }
-    });
+  });
+
+  it("it should not Create a bike without id ", (done) => {
+    let bike = {
+      color: "blue",
+      model: "34",
+    };
+
+    chai
+      .request(server)
+      .post("/")
+      .send(bike)
+      .end((err, res) => {
+        console.log(JSON.stringify(res));
+        res.should.have.status(405);
+        res.body.should.be.a("object");
+        done();
+      });
   });
 });
